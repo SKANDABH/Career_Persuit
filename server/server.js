9edb3e2231034d1219848d282bff9dd9dd2a062b
SKANDABH <115893732+SKANDABH@users.noreply.github.com>
2024-02-12 22:50:40 +0530

    creat main directory with basic code

---
+++ server/server.js
@@ -0,0 +1,5 @@
+const express =require("express");
+const cors= require("cors");
+const { application } = require("express");
+const portno=3000;
+app.post(portno,console.log("The server has started int port :"));
